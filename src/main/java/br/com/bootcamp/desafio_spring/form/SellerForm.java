0b991fd8116d76b0a7577b0f7ac620170137a028
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-06 17:27:39 -0300

    Criação do Form para o vendedor

---
+++ src/main/java/br/com/bootcamp/desafio_spring/form/SellerForm.java
@@ -0,0 +1,16 @@
+package br.com.bootcamp.desafio_spring.form;
+
+public class SellerForm {
+    private String name;
+
+    public SellerForm() {
+    }
+
+    public SellerForm(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
