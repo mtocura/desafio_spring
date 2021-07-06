71e309b98dd5b16dedf125fc8564d65a3fc2299d
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-06 18:46:15 -0300

    Criação de ExceptionDTO

---
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/ExceptionDTO.java
@@ -0,0 +1,14 @@
+package br.com.bootcamp.desafio_spring.dto;
+
+public class ExceptionDTO {
+
+    private String mensagem;
+
+    public ExceptionDTO(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
---
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/PostDTO.java
@@ -0,0 +1,101 @@
+package br.com.bootcamp.desafio_spring.entity;
+
+import java.util.Date;
+
+public class Post implements IEntity {
+    private Integer ID;
+    private Double price;
+    private Integer category;
+    private Product product;
+    private Integer sellerID;
+    private Boolean hasPromo;
+    private Double discount;
+    private Date expireAt;
+    private Date createdAt;
+
+    public Post(Integer ID, Double price, Integer category, Product product, Integer sellerID, Boolean hasPromo, Double discount, Date expireAt, Date createdAt) {
+        this.ID = ID;
+        this.price = price;
+        this.category = category;
+        this.product = product;
+        this.sellerID = sellerID;
+        this.hasPromo = hasPromo;
+        this.discount = discount;
+        this.expireAt = expireAt;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public Integer getID() {
+        return ID;
+    }
+
+    @Override
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getCategory() {
+        return category;
+    }
+
+    public void setCategory(Integer category) {
+        this.category = category;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(Integer sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public Boolean getHasPromo() {
+        return hasPromo;
+    }
+
+    public void setHasPromo(Boolean hasPromo) {
+        this.hasPromo = hasPromo;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(Date expireAt) {
+        this.expireAt = expireAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
