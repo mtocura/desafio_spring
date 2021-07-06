d9212064cb1f9169eeddf588cbe2ed02da51b0c5
João Lucas Nunes E Silva <joao.nunes@mercadolivre.com>
2021-07-06 17:27:48 -0300

    [Broken] Criar classe Repository
    A classe abstrata Repository serve de base para os repositórios
    de entidades.
    
    Nesse commit a classe não está funcional devido à ausência da
    dependências no projeto.

---
+++ src/main/java/br/com/bootcamp/desafio_spring/repository/Respository.java
@@ -0,0 +1,62 @@
+package br.com.bootcamp.desafio_spring.repository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Respository<T extends IEntity> {
+    private int nextID;
+
+    public T getById(int id) throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+        return entities.get(id);
+    }
+
+    public List<T> getById(List<Integer> ids) throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+        List<T> res = new ArrayList<>();
+        for (int id : ids) {
+            res.add(entities.get(id));
+        }
+        return res;
+    }
+
+    public List<T> getAll() throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+        return new ArrayList<>(entities.values());
+    }
+
+    public int save(T p) throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+
+        int id = nextID++;
+        p.setId(id);
+
+        entities.put(id, p);
+        Storage.getInstance().flush();
+        return id;
+    }
+
+    public int update(T p) throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+
+        entities.put(p.getId(), p);
+        Storage.getInstance().flush();
+        return p.getId();
+    }
+
+    public void remove(int id) throws IOException {
+        State s = Storage.getInstance().load();
+        Map<Integer, T> entities = this.getState(s);
+        entities.remove(id);
+        Storage.getInstance().flush();
+    }
+
+    protected abstract Map<Integer, T> getState(State s);
+}
