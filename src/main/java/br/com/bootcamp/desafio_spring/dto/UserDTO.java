f2c90fdac315a3b7c7de0205bafc35fb3e68a8a9
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-06 19:35:14 -0300

    Criação do UserDTO

--- src/main/java/br/com/bootcamp/desafio_spring/dto/UserDTO.java
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/UserDTO.java
@@ -2,18 +2,21 @@ package br.com.bootcamp.desafio_spring.dto;
 
 import java.util.List;
 
-public class UserFollowersCountDTO {
+public class UserDTO {
     private Integer id;
     private String name;
     private List<SellerDTO> following;
 
-    public UserFollowersCountDTO() {
-    }
 
-    public UserFollowersCountDTO(Integer id, String name, List<SellerDTO> following) {
+    public UserDTO(Integer id, String name, List<SellerDTO> following) {
         this.id = id;
         this.name = name;
         this.following = following;
+    }
+
+    public UserDTO(Integer id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public Integer getId() {
