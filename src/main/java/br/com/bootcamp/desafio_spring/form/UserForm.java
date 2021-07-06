abb49482a49dc0ee677ef1be84ce967e6c307fa0
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-06 17:26:52 -0300

    Criação do Form para o usuário

---
+++ src/main/java/br/com/bootcamp/desafio_spring/form/UserForm.java
@@ -0,0 +1,16 @@
+package br.com.bootcamp.desafio_spring.form;
+
+public class UserForm {
+    private String name;
+
+    public UserForm() {
+    }
+
+    public UserForm(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
