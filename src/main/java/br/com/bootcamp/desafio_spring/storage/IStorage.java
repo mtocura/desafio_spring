44c8e43ede79c7b6288c1364e757bcc326ab279d
amaiate <adriana.maiate@mercadolivre.com>
2021-07-06 17:30:25 -0300

    Incluindo classe IStorage

--- src/main/java/br/com/bootcamp/desafio_spring/storage/IStorage.java
+++ src/main/java/br/com/bootcamp/desafio_spring/storage/IStorage.java
@@ -3,5 +3,7 @@ package br.com.bootcamp.desafio_spring.storage;
 import java.io.IOException;
 
 public interface IStorage {
+    State load() throws IOException;
+    void flush() throws IOException;
 
 }
