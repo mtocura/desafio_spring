ee0c4c06c07744300bc1d200eac7daa9794b59ab
amaiate <adriana.maiate@mercadolivre.com>
2021-07-07 10:50:19 -0300

    Incluindo dependencia SellerService no Controller

--- src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
@@ -1,7 +1,20 @@
 package br.com.bootcamp.desafio_spring.controller;
 
+import br.com.bootcamp.desafio_spring.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SellerController {
+
+    private SellerService sellerService;
+
+    public SellerController() {
+    }
+
+    @Autowired
+    public SellerController(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
 }
