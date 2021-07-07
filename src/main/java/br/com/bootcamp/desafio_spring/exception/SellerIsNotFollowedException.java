d43e65d731cb3737ec0b27dc73e6ec58174d60bd
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-07 14:58:14 -0300

    Criação do Exception para serviço de Unfollow

--- src/main/java/br/com/bootcamp/desafio_spring/exception/SellerIsNotFollowedException.java
+++ src/main/java/br/com/bootcamp/desafio_spring/exception/SellerIsNotFollowedException.java
@@ -1,2 +1,11 @@
-package br.com.bootcamp.desafio_spring.exception;public class SellerIsNotFollowedException {
+package br.com.bootcamp.desafio_spring.exception;
+
+public class SellerIsNotFollowedException extends RuntimeException{
+
+    public SellerIsNotFollowedException() {
+    }
+
+    public SellerIsNotFollowedException(String message) {
+        super(message);
+    }
 }
