d9a4e4d5f37ffccebc34de4ca2635aa0e16191b9
amaiate <adriana.maiate@mercadolivre.com>
2021-07-07 17:25:55 -0300

    Incluindo Método Get followers list na controller

--- src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
@@ -2,11 +2,11 @@ package br.com.bootcamp.desafio_spring.controller;
 
 import br.com.bootcamp.desafio_spring.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/sellers")
 public class SellerController {
 
     private SellerService sellerService;
@@ -17,6 +17,11 @@ public class SellerController {
     @Autowired
     public SellerController(SellerService sellerService) {
         this.sellerService = sellerService;
+    }
+
+    @GetMapping("/users/{userId}/followers/list")
+    public ResponseEntity<?> getFollowedList(@PathVariable int userId) {
+        return ResponseEntity.ok(sellerService.followerList(userId));
     }
 
 }
