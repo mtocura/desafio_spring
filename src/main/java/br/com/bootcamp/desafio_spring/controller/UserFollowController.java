f727ee01fdbbe2582bd4dd8d0367834dfcd5dd75
amaiate <adriana.maiate@mercadolivre.com>
2021-07-07 10:54:28 -0300

    Incluindo dependencia UserFollowService no Controller

--- src/main/java/br/com/bootcamp/desafio_spring/controller/UserFollowController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/UserFollowController.java
@@ -1,7 +1,19 @@
 package br.com.bootcamp.desafio_spring.controller;
 
+import br.com.bootcamp.desafio_spring.service.UserFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserFollowController {
+
+    private UserFollowService userFollowService;
+
+    public UserFollowController() {
+    }
+
+    @Autowired
+    public UserFollowController(UserFollowService userFollowService) {
+        this.userFollowService = userFollowService;
+    }
 }
