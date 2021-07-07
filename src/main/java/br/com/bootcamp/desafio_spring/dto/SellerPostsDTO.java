e4bcf27e56c49d1a8096157c446a3c01a1831e7b
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-07 10:09:33 -0300

    Criação do SellerPostsDTO

--- src/main/java/br/com/bootcamp/desafio_spring/dto/SellerPostsDTO.java
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/SellerPostsDTO.java
@@ -1,2 +1,30 @@
-package br.com.bootcamp.desafio_spring.dto;public class SellerPostsDTO {
+package br.com.bootcamp.desafio_spring.dto;
+
+import java.util.List;
+
+public class SellerPostsDTO {
+
+    private Integer userId;
+    private List<PostDTO> posts;
+
+    public SellerPostsDTO(Integer userId, List<PostDTO> posts) {
+        this.userId = userId;
+        this.posts = posts;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public List<PostDTO> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostDTO> posts) {
+        this.posts = posts;
+    }
 }
