6f47a80de3ea2333be6a103d3e10edad0c49d84f
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-07 10:08:56 -0300

    Criação do ProductDTO

--- src/main/java/br/com/bootcamp/desafio_spring/dto/ProductDTO.java
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/ProductDTO.java
@@ -1,36 +1,36 @@
 package br.com.bootcamp.desafio_spring.dto;
 
 public class ProductDTO {
-    private Integer id;
-    private String name;
+    private Integer product_id;
+    private String productName;
     private String type;
     private String brand;
     private String color;
     private String notes;
 
-    public ProductDTO(Integer id, String name, String type, String brand, String color, String notes) {
-        this.id = id;
-        this.name = name;
+    public ProductDTO(Integer product_id, String productName, String type, String brand, String color, String notes) {
+        this.product_id = product_id;
+        this.productName = productName;
         this.type = type;
         this.brand = brand;
         this.color = color;
         this.notes = notes;
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getProduct_id() {
+        return product_id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
     }
 
-    public String getName() {
-        return name;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
     public String getType() {
