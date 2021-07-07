5c094b86f6bfd87511e438b5ea7adc37bf1992d6
amaiate <adriana.maiate@mercadolivre.com>
2021-07-07 14:15:14 -0300

    Incluindo dependencias UserRepository e UserFollowRepository na classe

--- src/main/java/br/com/bootcamp/desafio_spring/service/SellerService.java
+++ src/main/java/br/com/bootcamp/desafio_spring/service/SellerService.java
@@ -1,7 +1,19 @@
 package br.com.bootcamp.desafio_spring.service;
 
+import br.com.bootcamp.desafio_spring.repository.UserFollowRepository;
+import br.com.bootcamp.desafio_spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SellerService {
+
+    private UserRepository userRepository;
+    private UserFollowRepository userFollowRepository;
+
+    @Autowired
+    public SellerService(UserRepository userRepository, UserFollowRepository userFollowRepository) {
+        this.userRepository = userRepository;
+        this.userFollowRepository = userFollowRepository;
+    }
 }
