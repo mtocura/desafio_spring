134ff2c345d175d4fa32e390bc6bfaed81237ddd
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-07 08:43:22 -0300

    Criação do atributo campo na ExceptionDTO

--- src/main/java/br/com/bootcamp/desafio_spring/dto/ExceptionDTO.java
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/ExceptionDTO.java
@@ -2,13 +2,23 @@ package br.com.bootcamp.desafio_spring.dto;
 
 public class ExceptionDTO {
 
+    private String campo;
     private String mensagem;
 
     public ExceptionDTO(String mensagem) {
+        super();
+        this.mensagem = mensagem;
+    }
+
+    public ExceptionDTO(String campo, String mensagem) {
+        this.campo = campo;
         this.mensagem = mensagem;
     }
 
     public String getMensagem() {
         return mensagem;
+    }
+    public String getCampo() {
+        return campo;
     }
 }
