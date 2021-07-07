4f1f09ff38cc006d1da75eb3553ba9e01d78b515
Angelo Salles <angelo.salles@mercadolivre.com>
2021-07-07 17:09:34 -0300

    controller e service do RF 002 (contador de seguidores)

--- src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
@@ -1,9 +1,14 @@
 package br.com.bootcamp.desafio_spring.controller;
 
+import br.com.bootcamp.desafio_spring.dto.SellerFollowersCountDTO;
 import br.com.bootcamp.desafio_spring.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -19,4 +24,8 @@ public class SellerController {
         this.sellerService = sellerService;
     }
 
+    @GetMapping("/users/{userId}/followers/count")
+    public SellerFollowersCountDTO followersCountDTO(@PathVariable int userId) {
+        return this.sellerService.sellerFollowersCount(userId);
+    }
 }
