5fc914b9bf5fffbfbb3f9cbc05830297d82f4b7f
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-07 10:06:26 -0300

    Criação do SellerFollowersCountDTO

--- src/main/java/br/com/bootcamp/desafio_spring/dto/SellerFollowersCountDTO.java
+++ src/main/java/br/com/bootcamp/desafio_spring/dto/SellerFollowersCountDTO.java
@@ -1,12 +1,12 @@
 package br.com.bootcamp.desafio_spring.dto;
 
-public class FollowersCountDTO {
+public class SellerFollowersCountDTO {
 
     private Integer userId;
     private String userName;
     private Integer followers_count;
 
-    public FollowersCountDTO(Integer userId, String userName, Integer followers_count) {
+    public SellerFollowersCountDTO(Integer userId, String userName, Integer followers_count) {
         this.userId = userId;
         this.userName = userName;
         this.followers_count = followers_count;
