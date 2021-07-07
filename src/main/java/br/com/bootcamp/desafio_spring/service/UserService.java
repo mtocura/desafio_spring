400caee92e66639c4c2a0944cbf42e4ef052f9a7
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-07 14:59:47 -0300

    [Broken] Ajuste de lógica para retornar uma lista de vendedores que um determinado usuário segue

--- src/main/java/br/com/bootcamp/desafio_spring/service/UserService.java
+++ src/main/java/br/com/bootcamp/desafio_spring/service/UserService.java
@@ -1,7 +1,9 @@
 package br.com.bootcamp.desafio_spring.service;
 
+import br.com.bootcamp.desafio_spring.dto.SellerPostsDTO;
 import br.com.bootcamp.desafio_spring.dto.UserFollowingListDTO;
 import br.com.bootcamp.desafio_spring.entity.User;
+import br.com.bootcamp.desafio_spring.entity.UserFollow;
 import br.com.bootcamp.desafio_spring.exception.DatabaseException;
 import br.com.bootcamp.desafio_spring.exception.UserNotExistException;
 import br.com.bootcamp.desafio_spring.repository.UserFollowRepository;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,7 +33,7 @@ public class UserService {
     /*
     Método que retorna uma lista de vendedores que um determinado usuário, seja ele vendedor ou não, segue.
      */
-    public List<UserFollowingListDTO> followingList(int userId) {
+    public List<UserFollowingListDTO> followedList(int userId) {
         try {
             User user = userRepository.getById(userId);
 
@@ -38,7 +41,36 @@ public class UserService {
                 throw new UserNotExistException("Usuário " + userId + " não encontrado");
             }
 
-            return userFollowRepository.getByUserId(userId);
+            List<UserFollow> followedSellersRelationship = userFollowRepository.getByUserId(userId);
+
+            List<User> followedSellers = new ArrayList<>();
+
+            for (UserFollow userFollow : followedSellersRelationship) {
+                User seller = userRepository.getById(userFollow.getID());
+
+                followedSellers.add(seller);
+            }
+
+
+            return UserHandler.convertFollowingUsers(userId, user.getName(), followedSellers);
+        } catch (IOException e) {
+            throw new DatabaseException("Falha ao tentar acessar o banco de dados.");
+            return null;
+        }
+    }
+
+    /*
+    Método que retorna uma lista de posts das duas últimas semanas dos vendedores que um usuário segueo
+     */
+    public SellerPostsDTO followingPosts(int userId) {
+        try {
+            User user = userRepository.getById(userId);
+
+            if(user == null) {
+                throw new UserNotExistException("Usuário " + userId + " não encontrado");
+            }
+
+
         } catch (IOException e) {
             throw new DatabaseException("Falha ao tentar acessar o banco de dados.");
             return null;
