e52e6330a3aaf8d5d374ad815f8981ecc1f1f398
Angelo Salles <angelo.salles@mercadolivre.com>
2021-07-07 10:21:38 -0300

    handlers convert de post e product criados

---
+++ src/main/java/br/com/bootcamp/desafio_spring/handler/PostFormHandler.java
@@ -0,0 +1,28 @@
+package br.com.bootcamp.desafio_spring.handler;
+
+import br.com.bootcamp.desafio_spring.entity.Post;
+import br.com.bootcamp.desafio_spring.form.PostForm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class PostFormHandler {
+
+    public static Post create(PostForm postForm) {
+        return new Post(
+                postForm.getPostId(),
+                postForm.getPrice(),
+                postForm.getCategory(),
+                ProductFormHandler.create(postForm.getDetail()),
+                postForm.getUserId(),
+                false,
+                0.0,
+                new Date(),
+                new Date()
+        );
+    }
+
+    public Map<String, String> validate(PostForm postForm) {
+        return null;
+    }
+}
---
+++ src/main/java/br/com/bootcamp/desafio_spring/handler/ProductFormHandler.java
@@ -0,0 +1,25 @@
+package br.com.bootcamp.desafio_spring.handler;
+
+import br.com.bootcamp.desafio_spring.entity.Product;
+import br.com.bootcamp.desafio_spring.form.ProductForm;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ProductFormHandler{
+
+    public static Product create(ProductForm productForm) {
+        return new Product(
+                productForm.getProductId(),
+                productForm.getProductName(),
+                productForm.getType(),
+                productForm.getBrand(),
+                productForm.getColor(),
+                productForm.getNotes()
+        );
+    }
+
+    public Map<String, String> validate(ProductForm productForm) {
+        return null;
+    }
+}
