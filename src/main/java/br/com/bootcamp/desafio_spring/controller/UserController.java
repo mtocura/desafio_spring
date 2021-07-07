58f97f2dc2ca32c1ababd30b68162628e849e856
amaiate <adriana.maiate@mercadolivre.com>
2021-07-07 10:51:57 -0300

    Incluindo dependencia UserService no Controller

--- src/main/java/br/com/bootcamp/desafio_spring/controller/UserController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/UserController.java
@@ -1,7 +1,19 @@
 package br.com.bootcamp.desafio_spring.controller;
 
+import br.com.bootcamp.desafio_spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+
+    public UserController() {
+    }
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
