73a24c562d0a964805b5bed135a94e6c874f5e38
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-08 08:47:33 -0300

    Criação do método para contar o número de publicações promocionais de um vendedor

--- src/main/java/br/com/bootcamp/desafio_spring/service/SellerService.java
+++ src/main/java/br/com/bootcamp/desafio_spring/service/SellerService.java
@@ -1,6 +1,8 @@
 package br.com.bootcamp.desafio_spring.service;
 
 import br.com.bootcamp.desafio_spring.dto.SellerFollowersCountDTO;
+import br.com.bootcamp.desafio_spring.dto.SellerPromoPostsCountDTO;
+import br.com.bootcamp.desafio_spring.entity.Post;
 import br.com.bootcamp.desafio_spring.entity.User;
 import br.com.bootcamp.desafio_spring.entity.UserFollow;
 import br.com.bootcamp.desafio_spring.exception.DatabaseException;
@@ -12,7 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class SellerService {
@@ -39,6 +46,29 @@ public class SellerService {
             List<UserFollow> sellerFollowers = this.userFollowRepository.getSellerFollowers(seller.getId());
             return new SellerFollowersCountDTO(seller.getId(), seller.getName(), sellerFollowers.size());
         } catch (IOException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public SellerPromoPostsCountDTO sellerPromoPostsCount(int userId) {
+        try {
+            User seller = userRepository.getById(userId);
+
+            if(seller == null) {
+                throw new UserNotExistException("Usuário " + userId + " não encontrado");
+            }
+
+            if(!seller.getIsSeller()) {
+                throw new UserIsNotSellerException("Usuário " + seller.getId() + " não é um vendedor");
+            }
+
+            Predicate<Post> isPromoAndNotExpired = post -> post.getHasPromo() && post.getExpireAt().after(new Date());
+
+            // somente os posts que são promocionais e que não estão expirados
+            List<Post> promoPosts = seller.getPosts().stream().filter(isPromoAndNotExpired).collect(Collectors.toList());
+
+            return new SellerPromoPostsCountDTO(seller.getId(), seller.getName(), promoPosts.size());
+        } catch(IOException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
