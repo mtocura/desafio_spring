271bdb8f9287886dcbb1524ddb9c79ddde395a4c
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-08 08:50:08 -0300

    Criação do endpoint para obter o número de publicações promocionais de um vendedor

--- src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
+++ src/main/java/br/com/bootcamp/desafio_spring/controller/SellerController.java
@@ -3,6 +3,7 @@ package br.com.bootcamp.desafio_spring.controller;
 import br.com.bootcamp.desafio_spring.dto.SellerFollowersCountDTO;
 import br.com.bootcamp.desafio_spring.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,10 @@ public class SellerController {
     @GetMapping("/users/{userId}/followers/count")
     public SellerFollowersCountDTO followersCountDTO(@PathVariable int userId) {
         return this.sellerService.sellerFollowersCount(userId);
+    }
+
+    @GetMapping("/products/{userId}/countPromo/")
+    public ResponseEntity<?> promoPostsCount(@PathVariable int userId) {
+        return ResponseEntity.ok(sellerService.sellerPromoPostsCount(userId));
     }
 }
