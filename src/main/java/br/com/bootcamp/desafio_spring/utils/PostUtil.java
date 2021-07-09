c8ab41d5a0ba0a67afd51bbfd941d8d6a1cb04d7
gundesamanda <amanda.gundes@mercadolivre.com>
2021-07-09 14:15:13 -0300

    Ajuste para criação de método estático

--- src/main/java/br/com/bootcamp/desafio_spring/utils/PostUtil.java
+++ src/main/java/br/com/bootcamp/desafio_spring/utils/PostUtil.java
@@ -1,2 +1,16 @@
-package br.com.bootcamp.desafio_spring.utils;public class PostUtil {
+package br.com.bootcamp.desafio_spring.utils;
+
+import br.com.bootcamp.desafio_spring.entity.Post;
+import br.com.bootcamp.desafio_spring.entity.User;
+import br.com.bootcamp.desafio_spring.exception.PostIdAlreadyExistsException;
+
+public class PostUtil {
+
+    public static void idAlreadyExists(User seller, Post newPost) {
+        for (Post post : seller.getPosts()) {
+            if (newPost.getId().equals(post.getId())) {
+                throw new PostIdAlreadyExistsException("O vendedor já possui um post com este ID.");
+            }
+        }
+    }
 }
